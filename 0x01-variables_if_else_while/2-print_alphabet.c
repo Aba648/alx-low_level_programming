b8f27465eac032e3b0cb3feb9cb654c841ee33e5
Aba648 <abakil.khadija25@gmail.com>
2023-06-16 13:16:12 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - the main function
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
+
+
+
+
