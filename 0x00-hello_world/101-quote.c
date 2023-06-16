2d30368d1391a615ec6e259883c34c0c03e3c342
Aba648 <abakil.khadija25@gmail.com>
2023-06-15 19:37:58 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,12 @@
 #include <unistd.h>
 /**
  *main - the main function
- *Return: the programm retuen 1
+ *Return: the programm retuen 0
  */
 int main(void)
 {
-	 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	 return (1);
+	fputs("and that piece of art is useful\"-Dora Korpar,2015-10-19", stdout);
+	return (1);
 }
+
+
