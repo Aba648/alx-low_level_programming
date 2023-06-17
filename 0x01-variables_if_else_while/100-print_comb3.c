733cd82ad19cfde5ef97d72ed11ac3f4026926cb
Aba648 <abakil.khadija25@gmail.com>
2023-06-16 17:47:28 -0700

    task100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - the main function
+ *Return: 0 ( success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9 ; i++)
+	{
+		for (j = i + 1 ; j < 10 ; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
