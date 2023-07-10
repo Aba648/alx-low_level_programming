cc5ac6b9d3f76ec6c69945b369e79469d69efb5b
Aba648 <abakil.khadija25@gmail.com>
2023-07-10 12:04:49 -0700

    creat an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i = 0;
 
-	s = malloc((size + 1) * sizeof(char));
+	s = malloc(size * sizeof(char));
 
 	if (size == 0 || s == NULL)
 		return (NULL);
